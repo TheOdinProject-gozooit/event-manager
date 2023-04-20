2ffd603f3f0047707526f2e6e0cc72f093bd2357
gozooit <gozoo.it@protonmail.com>
2023-04-20 14:03:46 +0200

    Pseudocode for Cleaning Zip Codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -10,5 +10,10 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than five digits, truncate it to the first five digits
+  # if the zip code is less than five digits, add zeros to the front until it becomes five digits
+
   puts "#{name} #{zipcode}"
 end
